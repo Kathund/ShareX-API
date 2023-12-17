b3b4edd5659f73c4badb30e1fe1136fe3f6acea1
Kath <55346310+Kathund@users.noreply.github.com>
2023-12-17 16:17:32 +1100

    style(prettier)

--- src/endpoints/file.ts
+++ src/endpoints/file.ts
@@ -1,28 +1,24 @@
-import { Application, Request, Response } from "express";
-import { apiMessage, errorMessage } from "../logger";
-import { existsSync } from "fs";
-import { join } from "path";
+import { Application, Request, Response } from 'express';
+import { apiMessage, errorMessage } from '../logger';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 export default (app: Application) => {
-  app.get("/:name", async (req: Request, res: Response) => {
+  app.get('/:name', async (req: Request, res: Response) => {
     try {
       const fileName = req.params.name;
       apiMessage(req.path, `User is trying to get a file - ${fileName}`);
-      const filePath = join(__dirname, "../", "files", fileName);
+      const filePath = join(__dirname, '../', 'files', fileName);
       if (!existsSync(filePath)) {
         errorMessage(`File ${fileName} not found`);
-        return res
-          .status(404)
-          .send({ sucsess: false, message: `File ${fileName} not found` });
+        return res.status(404).send({ sucsess: false, message: `File ${fileName} not found` });
       }
 
       apiMessage(req.path, `File ${fileName} found`);
       return res.sendFile(filePath);
     } catch (err) {
       errorMessage(err as string);
-      return res
-        .status(500)
-        .send({ sucsess: false, message: "Internal server error" });
+      return res.status(500).send({ sucsess: false, message: 'Internal server error' });
     }
   });
 };
--- src/logger.ts
+++ src/logger.ts
@@ -1,106 +1,106 @@
 const customLevels = { api: 0, error: 1, warn: 2, other: 3, max: 4 };
-import { createLogger, format, transports } from "winston";
+import { createLogger, format, transports } from 'winston';
 
 const timezone = () => {
-  return new Date().toLocaleString("en-US", {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
+  return new Date().toLocaleString('en-US', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
     hour12: false,
   });
 };
 
 const apiTransport = new transports.File({
-  level: "api",
-  filename: "./logs/api.log",
+  level: 'api',
+  filename: './logs/api.log',
 });
 
 const errorTransport = new transports.File({
-  level: "error",
-  filename: "./logs/error.log",
+  level: 'error',
+  filename: './logs/error.log',
 });
 
 const warnTransport = new transports.File({
-  level: "warn",
-  filename: "./logs/warn.log",
+  level: 'warn',
+  filename: './logs/warn.log',
 });
 
 const otherTransport = new transports.File({
-  level: "other",
-  filename: "./logs/other.log",
+  level: 'other',
+  filename: './logs/other.log',
 });
 
 const combinedTransport = new transports.File({
-  level: "max",
-  filename: "./logs/combined.log",
+  level: 'max',
+  filename: './logs/combined.log',
 });
 
 const consoleTransport = new transports.Console({
-  level: "max",
+  level: 'max',
 });
 
 const apiLogger = createLogger({
-  level: "api",
+  level: 'api',
   levels: customLevels,
   format: format.combine(
     format.timestamp({ format: timezone }),
     format.printf(({ timestamp, level, message }) => {
       return `[${timestamp}] ${level.toUpperCase()} > ${message}`;
-    }),
+    })
   ),
   transports: [apiTransport, combinedTransport, consoleTransport],
 });
 
 const errorLogger = createLogger({
-  level: "error",
+  level: 'error',
   levels: customLevels,
   format: format.combine(
     format.timestamp({ format: timezone }),
     format.printf(({ timestamp, level, message }) => {
       return `[${timestamp}] ${level.toUpperCase()} > ${message}`;
-    }),
+    })
   ),
   transports: [errorTransport, combinedTransport, consoleTransport],
 });
 
 const warnLogger = createLogger({
-  level: "warn",
+  level: 'warn',
   levels: customLevels,
   format: format.combine(
     format.timestamp({ format: timezone }),
     format.printf(({ timestamp, level, message }) => {
       return `[${timestamp}] ${level.toUpperCase()} > ${message}`;
-    }),
+    })
   ),
   transports: [warnTransport, combinedTransport, consoleTransport],
 });
 
 const otherLogger = createLogger({
-  level: "other",
+  level: 'other',
   levels: customLevels,
   format: format.combine(
     format.timestamp({ format: timezone }),
     format.printf(({ timestamp, level, message }) => {
       return `[${timestamp}] ${level.toUpperCase()} > ${message}`;
-    }),
+    })
   ),
   transports: [otherTransport, combinedTransport, consoleTransport],
 });
 const logger = {
   api: (endPoint: string, message: string) => {
-    apiLogger.log("api", `${endPoint} | ${message}`);
+    apiLogger.log('api', `${endPoint} | ${message}`);
   },
   error: (message: string) => {
-    errorLogger.log("error", message);
+    errorLogger.log('error', message);
   },
   warn: (message: string) => {
-    warnLogger.log("warn", message);
+    warnLogger.log('warn', message);
   },
   other: (message: string) => {
-    otherLogger.log("other", message);
+    otherLogger.log('other', message);
   },
 };
 
